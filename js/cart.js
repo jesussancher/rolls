eb1d0a9a9eb3cc149b41e086ef9514575f1f2fbc
jsancher <jsancher@ieee.org>
2020-06-24 20:28:17 -0500

    Cart adding

--- js/cart.js
+++ js/cart.js
@@ -24,47 +24,53 @@ itemCounter = function(){
 var itemCounter = 1
 
 
-let carrito= {
-    campesina: counterCampesina(),
-    rancherita: counterRancherita(),
-    picarona: counterPicarona(),
-    melosa: counterMelosa(),
-    granjerita: counterGranjerita(),
-    caprichosa: counterCaprichosa(),
-}
+// let carrito= {
+//     campesina: counterCampesina(),
+//     rancherita: counterRancherita(),
+//     picarona: counterPicarona(),
+//     melosa: counterMelosa(),
+//     granjerita: counterGranjerita(),
+//     caprichosa: counterCaprichosa(capi),
+// }
 
-function debug(){
-    const camp = counterCampesina()
-    return camp
-}
+// function debug(){
+//     const camp = counterCampesina()
+//     return camp
+// }
 
 let count = 0
 function addItem(id){
     const labelCart = document.getElementById("counterCart")
     // const campesina = document.getElementById("campesina")
     switch(id){
-        case 'addCampesina': 
-        count += counterCampesina()
+        case 'addCampesina':
+        counterCampesina(campesina) 
+        count ++ 
         break;
 
         case 'addPicarona': 
-        count += counterPicarona()
+        counterPicarona(picarona)
+        count ++
         break;
         
         case 'addRancherita': 
-        count += counterRancherita()
+        counterRancherita(rancherita)
+        count ++
         break;
 
-        case 'addMelosa': 
-        count += counterMelosa()
+        case 'addMelosa':
+        counterMelosa(melosa) 
+        count ++
         break;
 
         case 'addGranjerita': 
-        count += counterGranjerita()
+        counterGranjerita(granjerita)
+        count ++
         break;
 
         case 'addCaprichosa': 
-        count += counterCaprichosa()
+        counterCaprichosa(caprichosa)
+        count ++
         break;
     }
     const html = `${count}` 
@@ -73,44 +79,88 @@ function addItem(id){
     console.log(id)
 }
 // Campesina counteer
-var campesina = 1
-function counterCampesina(){
-    const camp = campesina++
+var campesina = {
+    name: "Campesina",
+    qty: 1
+}
+function counterCampesina(campi){
+    const camp = campi.qty++
     // console.log(camp)
     return camp
 }
 
 // Rancherita counter
-var rancherita = 1
-function counterRancherita(){
-    const ranch = rancherita++
+var rancherita = {
+    name: "Rancherita",
+    qty: 1
+}
+function counterRancherita(ranchi){
+    const ranch = ranchi.qty++
     return ranch
 }
 
 // Picarona counter 
-var picarona = 1
-function counterPicarona(){
-    const pic = picarona++
+var picarona = {
+    name: "Picarona",
+    qty: 1
+}
+function counterPicarona(pica){
+    const pic = pica.qty++
     return pic
 }
 
 // Melosa counter
-var melosa = 1
-function counterMelosa(){
-    const mel = melosa++
+var melosa = {
+    name: "Melosa",
+    qty: 1
+}
+function counterMelosa(capi){
+    const mel = capi.qty++
     return mel
 }
 
 // Granjerita counter
-var granjerita = 1
-function counterGranjerita(){
-    const granj = granjerita++
+var granjerita = {
+    name: "Granjerita",
+    qty: 1
+}
+function counterGranjerita(capi){
+    const granj = capi.qty++
     return granj
 }
 
 // Caprichosa counter
-var caprichosa = 1
-function counterCaprichosa(){
-    const cap = caprichosa++
-    return cap
+var caprichosa = {
+    name: "Caprichosa",
+    qty: 1
+}
+function counterCaprichosa(capi){
+    const qty = capi.qty++
+    // console.log(cap)
+    return qty
+}
+
+
+
+function pedido(){
+    const productos = [campesina,rancherita,melosa,picarona,granjerita,caprichosa]
+    let pedido = []
+    let psudoLink = ''
+    let pseudo = []
+    let link = ''
+    for (let i = 0; i<productos.length; i++){
+        pedido[i] = []
+        if (productos[i].qty > 1  ){
+            pedido[i].push(productos[i].qty-1,productos[i].name+"s")
+        } 
+
+    }
+
+    // for (let o = 0; o < pedido.length; o++){
+    //     if(o%2  == 0){
+    //         pseudo[o] = 
+    //     }
+    // }
+    const url = "https://api.whatsapp.com/send?phone=573183147984&text=%C2%A1Hola!%0AQuiero%3A%0A1%20Rancherita%2C%202%20Picaronas%20y%204%20Caprichosas.%0AGracias"
+    console.log(pedido)
 }
